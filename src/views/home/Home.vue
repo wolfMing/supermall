566f648e1a4cf66358975c9345b53f57b9d86adf
zsm <893159176@qq.com>
2020-04-24 15:37:22 +0800

    首页-轮播图、推荐信息展示，featureView、TabControl封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,20 +3,97 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-  <home-swiper :banners="banners"></home-swiper>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>00</li><li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>00</li><li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>00</li><li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>00</li><li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>00</li><li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>00</li><li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>00</li>
+    </ul>
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar'
   import HomeSwiper from './childrenComps/HomeSwiper'
+  import RecommendView from './childrenComps/RecommendView'
+  import FeatureView from './childrenComps/FeatureView'
+
+  import NavBar from 'components/common/navbar/NavBar'
+  import TabControl from 'components/content/tabControl/TabControl'
+
   import {getHomeMultidata} from "../../network/home"
 
   export default {
     name: "home",
     components: {
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
       NavBar,
-      HomeSwiper
+      TabControl,
     },
     data() {
       return {
@@ -36,8 +113,23 @@
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
   .home-nav {
     background-color: var(--color-tint);
     color: white;
+
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 9;
+  }
+
+  .tab-control {
+    /*当距离顶部的距离为44px时，修改position为fixed*/
+    position: sticky;
+    top: 44px;
   }
 </style>
